219d28e1a49e87b85fe22df9175148f273ffd498
Artem Nizamiiev <upuyayua@gmail.com>
2025-12-18 22:45:55 +0200

    solved2

--- network.tf
+++ network.tf
@@ -1,41 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.105.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-variable "prefix" {
-  type    = string
-  default = "tfvmex"
-}
-
-variable "location" {
-  type    = string
-  default = "West Europe"
-}
-
-variable "vm_count" {
-  type    = number
-  default = 3
-}
-
-variable "admin_username" {
-  type    = string
-  default = "azureuser"
-}
-
-variable "ssh_public_key" {
-  type        = string
-  description = "SSH public key content (e.g. starts with ssh-rsa or ssh-ed25519)"
-}
-
 locals {
   nic_names = [for i in range(var.vm_count) : "${var.prefix}-nic-${i}"]
 
@@ -76,11 +38,6 @@ locals {
   ]
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-rg"
-  location = var.location
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -99,7 +56,6 @@ resource "azurerm_network_security_group" "nsg" {
   name                = "${var.prefix}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
   dynamic "security_rule" {
     for_each = local.security_rules
     content {
@@ -133,58 +89,4 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   for_each                  = azurerm_network_interface.nic
   network_interface_id      = each.value.id
   network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  count               = var.vm_count
-  name                = "${var.prefix}-vm-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1s"
-
-  network_interface_ids = [
-    azurerm_network_interface.nic[local.nic_names[count.index]].id
-  ]
-
-  admin_username                  = var.admin_username
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  tags = {
-    environment = "staging"
-    owner       = "mateacademy"
-    project     = var.prefix
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-output "vm_name_uppercase" {
-  value = upper(azurerm_linux_virtual_machine.vm[0].name)
-}
-
-output "vm_tags_joined" {
-  value = join(", ", values(azurerm_linux_virtual_machine.vm[0].tags))
-}
-
-output "all_vm_ids" {
-  value = [for vm in azurerm_linux_virtual_machine.vm : vm.id]
 }
